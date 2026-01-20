d413c88a620ed761822d7efad8845d68f25b8356
Abhyuday <rajabhyuday01@gmail.com>
2026-01-20 23:59:29 +0530

    set online API base URL to production environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-listing-backend-production-234d.up.railway.app/api",
 });
 
 export const fetchJobs = (
